b8b36703524faf2112b422d947bb5f1910b72454
Jacob Brook <brookjacob@gmail.com>
2022-05-03 13:32:32 -0600

    Add buttons to populateDOM and related functions

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,7 +7,11 @@ function Book(author, title, numberOfPages, read) {
     this.read = read;
 
     this.changeReadStatus = function() {
-        this.read = !read;
+        if (this.read == true) {
+            this.read = false;
+        } else {
+            this.read = true;
+        }
     }
 }
 
@@ -23,35 +27,153 @@ addBookToLibrary("Fyodor Dostoevsky", "Crime and Punishment", "650", true);
 addBookToLibrary("Julia Leigh", "The Hunter", "170", false);
 
 function populateDOM(library) {
+
+    //Reset book DOM
+    const content = Array.from(document.getElementsByClassName('content'));
+
+    content.forEach(book => {
+        book.remove();
+    })
+
+    //Add library array to DOM
     const books = document.getElementsByClassName('books')[0];
+
     library.forEach(book => {
+        //Create Book Container
         const content = document.createElement('div');
         content.classList.add('content');
+        content.dataset.index = myLibrary.indexOf(book);
 
+        //Create Book Info Container
+        const bookInfo = document.createElement('div');
+        bookInfo.classList.add('book-info');
+
+        //Create Title for Book Info Container
         const title = document.createElement('h3');
         title.classList.add('content-title');
         title.innerText = book.title;
-        content.appendChild(title);
+        bookInfo.appendChild(title);
 
+        //Create Author for Book Info Container
         const author = document.createElement('p');
         author.classList.add('content-author');
         author.innerText = book.author;
-        content.appendChild(author);
+        bookInfo.appendChild(author);
 
+        //Create Number of Pages for Book Info Container
         const numberOfPages = document.createElement('p');
         numberOfPages.classList.add('content-pages');
         numberOfPages.innerText = book.numberOfPages;
-        content.appendChild(numberOfPages);
+        bookInfo.appendChild(numberOfPages);
+
+        //Create Book Buttons Container for Book Container
+        const bookButtons = document.createElement('div');
+        bookButtons.classList.add('book-buttons');
+        //Create Read/Not Read Button for Book Buttons Container
+        const readButton = document.createElement('button');
+        readButton.classList.add('read-button');
+        readButton.innerText = "Read";
+        bookButtons.appendChild(readButton);
+        //Create Remove Button for Book Buttons Container
+        const removeButton = document.createElement('button');
+        removeButton.classList.add('remove-button');
+        removeButton.innerText = "-";
+        bookButtons.appendChild(removeButton);
+
+        content.appendChild(bookInfo)
+        content.appendChild(bookButtons);
 
         if (book.read == true) {
-            content.style.backgroundColor = "#78A1BB";
+            readButton.classList.add('read');
+            readButton.innerText = "Read";
         } else {
-            content.style.backgroundColor = "#EBF5EE";
+            readButton.classList.add('not-read');
+            readButton.innerText = "Not Read";
         }
 
         books.appendChild(content);
-
     });
+
+    const removeBook = Array.from(document.getElementsByClassName('remove-button'));
+    removeBook.forEach(button => {
+        button.addEventListener('mouseup', function() {
+            const book = myLibrary[button.parentNode.parentNode.dataset.index];
+            console.log(book);
+            const index = myLibrary.indexOf(book);
+            console.log(index);
+            if (index > -1) {
+                myLibrary.splice(index, 1);
+            }
+            populateDOM(myLibrary);
+            console.log(removeBook.length)
+        });
+    })
 }
 
+
+//element selectors for controlling the modal
+const modal = document.getElementsByClassName('modal')[0];
+const openModal = document.getElementsByClassName('add')[0];
+const closeModal = document.getElementsByClassName('close')[0];
+
+openModal.addEventListener('mouseup', function() {
+    modal.style.display = "block";
+})
+
+closeModal.addEventListener('mouseup', function() {
+    modal.style.display = "none";
+})
+
+window.addEventListener('mouseup', function() {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+})
+
+
+//Add Book To myLibrary
+const addBook = document.getElementsByClassName('confirm')[0];
+
+addBook.addEventListener('mouseup', function() {
+    const authorInput = document.getElementById('author').value;
+    const titleInput = document.getElementById('title').value;
+    const pagesInput = document.getElementById('pages').value;
+    const readInput = document.querySelector('input[name="read"]:checked').value;
+    addBookToLibrary(authorInput, titleInput, pagesInput, readInput);
+    populateDOM(myLibrary)
+    modal.style.display = "none";
+})
+
 populateDOM(myLibrary);
+
+const readBook = Array.from(document.getElementsByClassName('read-button'));
+readBook.forEach(button => {
+    button.addEventListener('mouseup', function() {
+        const book = myLibrary[button.parentNode.parentNode.dataset.index];
+        book.changeReadStatus();
+        if (book.read == true) {
+            button.classList.remove('not-read');
+            button.classList.add('read');
+            button.innerText = "Read";
+        } else {
+            button.classList.add('not-read');
+            button.classList.remove('read');
+            button.innerText = "Not Read";
+        }
+    });
+});
+
+const removeBook = Array.from(document.getElementsByClassName('remove-button'));
+removeBook.forEach(button => {
+    button.addEventListener('mouseup', function() {
+        const book = myLibrary[button.parentNode.parentNode.dataset.index];
+        console.log(book);
+        const index = myLibrary.indexOf(book);
+        console.log(index);
+        if (index > -1) {
+            myLibrary.splice(index, 1);
+        }
+        populateDOM(myLibrary);
+        console.log(removeBook.length)
+    });
+})
